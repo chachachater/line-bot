66d79a64734fc59c02f58107bbecc843dd5120eb
Sydars <xsw5888xsw@gmail.com>
2021-08-09 23:25:33 +0800

    2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var bot = linebot({
   channelId: process.env.CHANNEL_ID,
   channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  verify: true // Verify 'X-Line-Signature' header (default=true)
+  // verify: true // Verify 'X-Line-Signature' header (default=true)
 })
 
 bot.on('message', function (event) {
