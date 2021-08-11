485b0232a62300a73837d3d7e9a4050016664fd6
Sydars <xsw5888xsw@gmail.com>
2021-08-12 00:21:01 +0800

    add twint

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
-var linebot = require('linebot')
+const linebot = require('linebot')
+const utils = require('./utils')
+
 const app = express()
 const port = process.env.PORT || 3000
 
-var bot = linebot({
+const bot = linebot({
   channelId: process.env.CHANNEL_ID,
   channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -12,14 +14,16 @@ var bot = linebot({
 const linebotParser = bot.parser()
 app.post('/linewebhook', linebotParser)
 
-app.get('/line-bot', (req, res, next) => {
-  console.log('/line-bot')
+app.get('/line-bot', async (req, res, next) => {
+  const data =await utils.getTweets()
+  console.log(`這是最新的三篇推特:${data.tweets0.tweet}, ${data.tweets0.thumbnail}, ${data.tweets1.tweet}, ${data.tweets1.thumbnail}, ${data.tweets2.tweet}, ${data.tweets2.thumbnail}`)
   res.end('urrr')
 })
 
-bot.on('message', function (event) {
+bot.on('message', async function (event) {
   console.log('we got a message', event)
-  event.reply(`did you say:${event.message.text}`).then(function (resp) {
+  const data = await utils.getTweets()
+  event.reply(`這是最新的三篇推特:${data.tweets0.tweet}, ${data.tweets0.thumbnail}, ${data.tweets1.tweet}, ${data.tweets1.thumbnail}, ${data.tweets2.tweet}, ${data.tweets2.thumbnail}`).then(function (resp) {
     console.log('we success')
     console.log('resp', resp)
   }).catch(function (error) {
